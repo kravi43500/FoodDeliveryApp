b66a31926190a1819615bef99c5ec95793ad2215
Ravi Kumar <kravi4350@gmail.com>
2022-10-03 11:42:06 +0530

    Updating logics

---
+++ build/precache-manifest.9f287e9fbc7f0f1af7663304617e7326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711aec3b84029a223aafb27ccbdd9b9d",
+    "url": "/MeghadootElectronics/index.html"
+  },
+  {
+    "revision": "94e9752cd7679039070d",
+    "url": "/MeghadootElectronics/static/js/2.05f23e24.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/MeghadootElectronics/static/js/2.05f23e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1dc7232b87068c1bcb2",
+    "url": "/MeghadootElectronics/static/js/main.d681b4eb.chunk.js"
+  },
+  {
+    "revision": "dafcc519ee5a8a21ab27",
+    "url": "/MeghadootElectronics/static/js/runtime-main.913030ad.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/MeghadootElectronics/static/media/commerce.422fec10.png"
+  }
+]);
